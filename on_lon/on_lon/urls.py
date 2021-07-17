59d60cefef549580975ece0cc7b18d6a2743f7d3
Ryukyo <florian.fejer@hotmail.de>
2021-07-17 17:16:57 +0900

    Set up db models

--- on_lon/on_lon/urls.py
+++ on_lon/on_lon/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+API_PATH = 'api/v1/'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('djoser.urls')),
-    path('api/v1/', include('djoser.urls.authtoken')),
-]
+    path(API_PATH, include('djoser.urls')),
+    path(API_PATH, include('djoser.urls.authtoken')),
+    path(API_PATH, include('product.urls')),
+    path(API_PATH, include('order.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
