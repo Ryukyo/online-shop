ee529d3f3f3a442b93ef2221a2de4f38de2c1071
Ryukyo <florian.fejer@hotmail.de>
2021-07-18 11:53:09 +0900

    Add basic name and desc search functionality

--- on-lon_vue/src/App.vue
+++ on-lon_vue/src/App.vue
@@ -1,10 +1,146 @@
 <template>
   <div id="wrapper">
-    <router-view />
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>On-Lon</strong></router-link>
+        <!-- Show burger in mobile view; open menu on click -->
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
 
+      <!-- Toggle 'is active' class when burger menu is clicked -->
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu }">
+        <div class="navbar-start">
+          <div class="navbar-item">
+            <form method="get" action="/search">
+              <div class="field has-addons">
+                <div class="control">
+                  <input type="text" class="input" placeholder="What are you looking for?" name="query">
+                </div>
+
+                <div class="control">
+                  <button class="button is-success">
+                      <span class="icon">
+                      <em class="fas fa-search"></em>
+                      </span>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <div class="navbar-end">
+          <router-link to="/drones" class="navbar-item">Drones</router-link>
+          <router-link to="/accessoires" class="navbar-item">Accessories</router-link>
+
+          <div class="navbar-item">
+            <div class="buttons">
+              <template v-if="$store.state.isAuthenticated">
+                <router-link to="/my-account" class="button is-light">My Account</router-link>
+              </template>
+
+              <template v-else>
+                <router-link to="/log-in" class="button is-light">Log in</router-link>
+              </template>
+
+              <router-link to="/cart" class="button is-success">
+                <span class="icon"><em class="fas fa-shopping-cart"></em></span>
+                <span>Cart ({{ cartTotalLength }})</span>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <div class="is-loading-bar has-text-centered" v-bind:class="{'is-loading': $store.state.isLoading }">
+      <div class="lds-dual-ring"></div>
+    </div>
+
+    <section class="section">
+      <router-view/>
+    </section>
+
+    <footer class="footer">
+      <p class="has-text-centered">This shop is fictional</p>
+    </footer>
   </div>
 </template>
 
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      showMobileMenu: false,
+      cart: {
+        items: []
+      }
+    }
+  },
+  beforeCreate() {
+    this.$store.commit('initializeStore')
+    const token = this.$store.state.token
+    if (token) {
+        axios.defaults.headers.common['Authorization'] = "Token " + token
+    } else {
+        axios.defaults.headers.common['Authorization'] = ""
+    }
+  },
+  mounted() {
+    this.cart = this.$store.state.cart
+  },
+  computed: {
+      cartTotalLength() {
+          let totalLength = 0
+          for (let item of this.cart.items) {
+            totalLength += item.quantity
+          }
+          return totalLength
+      }
+  }
+}
+</script>
+
+
 <style lang="scss">
-@import'../node_modules/bulma
+@import '../node_modules/bulma';
+.lds-dual-ring {
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+}
+.lds-dual-ring:after {
+  content: " ";
+  display: block;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border-radius: 50%;
+  border: 6px solid #ccc;
+  border-color: #ccc transparent #ccc transparent;
+  animation: lds-dual-ring 1.2s linear infinite;
+}
+@keyframes lds-dual-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+.is-loading-bar {
+  height: 0;
+  overflow: hidden;
+  -webkit-transition: all 0.3s;
+  transition: all 0.3s;
+  &.is-loading {
+    height: 80px;
+  }
+}
 </style>
